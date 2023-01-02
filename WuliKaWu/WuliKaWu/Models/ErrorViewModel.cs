2c7fc173b19198643ab876a3ed3e385c16620bdf
Jim Chiu <liang.case@gmail.com>
2023-01-02 15:43:53 +0800

    建立 Core MVC 專案作為基底範本

---
+++ WuliKaWu/WuliKaWu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WuliKaWu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
