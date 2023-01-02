53cadfe7a54e35e64e2d5ab65a880ed956d1b286
Jim Chiu <liang.case@gmail.com>
2023-01-03 00:26:18 +0800

    [套版] 於 HomeController 中預留 Action methods 給樣版頁面，來產生對應的 MVC 檢視

--- WuliKaWu/WuliKaWu/Controllers/HomeController.cs
+++ WuliKaWu/WuliKaWu/Controllers/HomeController.cs
@@ -15,7 +15,115 @@ namespace WuliKaWu.Controllers
             _logger = logger;
         }
 
+        // TODO: 請整合 about-us.html
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 blog.html
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 blog-details.html
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 blog-sidebar.html
+        public IActionResult BlogSidebar()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 cart.html
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 checkout.html
+        [ActionName("Checkout")]
+        public IActionResult CheckoutCart()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 compare.html
+        [ActionName("Compare")]
+        public IActionResult CompareProducts()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 contact-us.html
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 首頁
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-2.html
+        public IActionResult IndexTwo()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-3.html
+        public IActionResult IndexThree()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-4.html
+        public IActionResult IndexFour()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-5.html
+        public IActionResult IndexFive()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-6.html
+        public IActionResult IndexSix()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-7.html
+        public IActionResult IndexSeven()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 index-8.html
+        public IActionResult IndexEight()
+        {
+            return View();
+        }
+
+        // TODO: 請整合至 Indentity 控制頁面
+        //public IActionResult LoginRegister()
+        //{
+        //    return View();
+        //}
+
+        // TODO: 請整合 my-account.html
+        public IActionResult MyAccount()
         {
             return View();
         }
@@ -30,6 +138,78 @@ namespace WuliKaWu.Controllers
         /// </summary>
         /// <returns></returns>
         public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 product-details-2.html
+        public IActionResult ProductDetailsTwo()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 product-details-affiliate.html
+        public IActionResult ProductDetailsAffiliate()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 product-details-fixed-img.html
+        public IActionResult ProductDetailsFixedImg()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 product-details-gallery.html
+        public IActionResult ProductDetailsGallery()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 product-details-group.html
+        public IActionResult ProductDetailsGroup()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shop.html
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shoplist.html
+        public IActionResult ShopList()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shop-list-sidebar.html
+        public IActionResult ShopListSidebar()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shop-location.html
+        public IActionResult ShopLocation()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shop-right-sidebar.html
+        public IActionResult ShopRightSidebar()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 shop-sidebar.html
+        public IActionResult ShopSidebar()
+        {
+            return View();
+        }
+
+        // TODO: 請整合 wishlist.html
+        public IActionResult WishList()
         {
             return View();
         }
