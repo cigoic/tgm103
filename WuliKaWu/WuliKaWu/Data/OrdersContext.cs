00ba4517814e95a040a3a7c6148fc2be3e3fbb11
Tibame_T14_05_SandraLee <jame6208@gmail.com>
2023-01-07 15:11:01 +0800

    [新增]UpdateOrders[Data,Models]Context

---
+++ WuliKaWu/WuliKaWu/Data/OrdersContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WuliKaWu.Models;
+
+namespace WuliKaWu.Data
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext(DbContextOptions<OrdersContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Orders> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Orders>().ToTable("Orders");
+        }
+    }
+}
---
+++ WuliKaWu/WuliKaWu/Models/Orders.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WuliKaWu.Models
+{
+    [Table("Orders")]
+    public class Orders
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        public string ProductName { get; set; }
+        public int Amount { get; set; }
+        public int UnitPrice { get; set; }
+        public float? Discount { get; set; }
+        public string ShippingAddress { get; set; }
+
+        public enum GetPayType
+        { Cash, CreditCard, MoblilePay }
+
+        public string? Memo { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
