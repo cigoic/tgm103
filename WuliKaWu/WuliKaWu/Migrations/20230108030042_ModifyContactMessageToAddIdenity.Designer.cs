991c3cdc548a9217790b4fbfb15de783699348dd
Jim Chiu <liang.case@gmail.com>
2023-01-08 11:33:37 +0800

    [新增] Contact Us 控制器與檢視、Model

--- WuliKaWu/WuliKaWu/Migrations/20230108030042_ModifyContactMessageToAddIdenity.Designer.cs
+++ WuliKaWu/WuliKaWu/Migrations/20230108030042_ModifyContactMessageToAddIdenity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WuliKaWu.Models;
 
@@ -11,9 +12,10 @@ using WuliKaWu.Models;
 namespace WuliKaWu.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20230108030042_ModifyContactMessageToAddIdenity")]
+    partial class ModifyContactMessageToAddIdenity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
