dde7071237cf72b47d528ced38e4dd6afd302311
Tibame_T14_05_SandraLee <jame6208@gmail.com>
2023-01-18 11:22:14 +0800

    Merge branch 'development' of https://github.com/cigoic/tgm103 into development
    # Conflicts:
    #	MVCNetFramework/MVCNetFramework/MVCNetFramework.csproj

--- WuliKaWu/WuliKaWu/Controllers/CartController.cs
+++ WuliKaWu/WuliKaWu/Controllers/CartController.cs
@@ -12,13 +12,11 @@ namespace WuliKaWu.Controllers
 {
     public class CartController : Controller
     {
-        private readonly ShopContext _context;
-
-        public CartController(ShopContext context)
+        public IActionResult AddCart(Data.Product product)
         {
-            _context = context;
         }
 
+<<<<<<< HEAD
         // GET: Cart
         public async Task<IActionResult> Index()
         {
@@ -177,5 +175,186 @@ namespace WuliKaWu.Controllers
         {
             return _context.Carts.Any(e => e.Id == id);
         }
+=======
+        //    private readonly ShopContext _context;
+
+        //    public CartController(ShopContext context)
+        //    {
+        //        _context = context;
+        //    }
+
+        //    // GET: Orders
+        //    public async Task<IActionResult> Index()
+        //    {
+        //        return View(await _context.Orders.ToListAsync());
+        //    }
+
+        //    //  TODO GET: Cart
+
+        //    public async Task<IActionResult> GetCartDetailsAsync()
+        //    {
+        //        //var cvm = _context.Carts.Where(
+        //        //    vm => vm.Id == id).Select(vm => new CartViewModel
+
+        //        //    {
+        //        //        //Id = vm.Id,
+        //        //        Conutry = vm.Conutry,
+        //        //        State = vm.State,
+        //        //        City = vm.City,
+        //        //        PostalCode = vm.PostalCode,
+        //        //        Picture = vm.Picture,
+        //        //        ProductName = vm.ProductName,
+        //        //        Price = vm.Price,
+        //        //        Quantity = vm.Quantity,
+        //        //        CouponDiscount = vm.CouponDiscount,
+        //        //    });
+
+        //        return View();
+        //    }
+
+        //    //public async Task<IActionResult> GetCartDetailsAsync(int pid, int oid)
+        //    //{
+        //    //    var cvm = new CartViewModel();
+        //    //    var product = await _context.Products.Where(p => p.ProductId == pid).FirstOrDefaultAsync();
+        //    //    cvm.ProductName = product.ProductName;
+        //    //    var order = await _context.Orders.Where(o => o.OrderId == oid).FirstOrDefaultAsync();
+        //    //    cvm.Price = order.UnitPrice;
+
+        //    //    return View(cvm);
+        //    //}
+
+        //    // GET: Orders/Details/5
+        //    public async Task<IActionResult> Details(int? id)
+        //    {
+        //        if (id == null || _context.Orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        var orders = await _context.Orders
+        //            .FirstOrDefaultAsync(m => m.OrderId == id);
+        //        if (orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        return View(orders);
+        //    }
+
+        //    // GET: Orders/Create
+        //    public IActionResult Create()
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: Orders/Create
+        //    // To protect from overposting attacks, enable the specific properties you want to bind to.
+        //    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> Create([Bind("OrderId,ProductName,Amount,UnitPrice,Discount,ShippingAddress,Memo")] Orders orders)
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            _context.Add(orders);
+        //            await _context.SaveChangesAsync();
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        return View(orders);
+        //    }
+
+        //    // GET: Orders/Edit/5
+        //    public async Task<IActionResult> Edit(int? id)
+        //    {
+        //        if (id == null || _context.Orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        var orders = await _context.Orders.FindAsync(id);
+        //        if (orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+        //        return View(orders);
+        //    }
+
+        //    // POST: Orders/Edit/5
+        //    // To protect from overposting attacks, enable the specific properties you want to bind to.
+        //    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //    [HttpPost]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> Edit(int id, [Bind("OrderId,ProductName,Amount,UnitPrice,Discount,ShippingAddress,Memo")] Orders orders)
+        //    {
+        //        if (id != orders.OrderId)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        if (ModelState.IsValid)
+        //        {
+        //            try
+        //            {
+        //                _context.Update(orders);
+        //                await _context.SaveChangesAsync();
+        //            }
+        //            catch (DbUpdateConcurrencyException)
+        //            {
+        //                if (!OrdersExists(orders.OrderId))
+        //                {
+        //                    return NotFound();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        return View(orders);
+        //    }
+
+        //    // GET: Orders/Delete/5
+        //    public async Task<IActionResult> Delete(int? id)
+        //    {
+        //        if (id == null || _context.Orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        var orders = await _context.Orders
+        //            .FirstOrDefaultAsync(m => m.OrderId == id);
+        //        if (orders == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        return View(orders);
+        //    }
+
+        //    // POST: Orders/Delete/5
+        //    [HttpPost, ActionName("Delete")]
+        //    [ValidateAntiForgeryToken]
+        //    public async Task<IActionResult> DeleteConfirmed(int id)
+        //    {
+        //        if (_context.Orders == null)
+        //        {
+        //            return Problem("Entity set 'ShopContext.Orders'  is null.");
+        //        }
+        //        var orders = await _context.Orders.FindAsync(id);
+        //        if (orders != null)
+        //        {
+        //            _context.Orders.Remove(orders);
+        //        }
+
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    private bool OrdersExists(int id)
+        //    {
+        //        return _context.Orders.Any(e => e.OrderId == id);
+        //    }
+>>>>>>> Merge branch 'development' of https://github.com/cigoic/tgm103 into development
     }
 }
