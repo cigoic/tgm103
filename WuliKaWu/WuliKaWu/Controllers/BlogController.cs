9c6fd516db5ce1baf7bc5f069f1df238202a0d67
Jim Chiu <liang.case@gmail.com>
2023-01-28 17:07:55 +0800

    [更新] Blog Index 檢視可顯示單筆部落格文章與圖片

--- WuliKaWu/WuliKaWu/Controllers/BlogController.cs
+++ WuliKaWu/WuliKaWu/Controllers/BlogController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using WuliKaWu.Data;
+using WuliKaWu.Models;
 
 namespace WuliKaWu.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly ShopContext _context;
+
+        public BlogController(ShopContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var articles = await _context.Articles.FirstOrDefaultAsync();
+
+            var vm = new ArticleModel();
+            vm.MemberName = _context.Members.Where(m => m.MemberId == articles.MemberId).FirstOrDefault().Name;
+            vm.FileName = _context.ArticleContentImages.Where(i => i.ArticleId == articles.ArticleId).FirstOrDefault().FileName;
+            vm.Title = _context.Articles.Where(i => i.ArticleId == articles.ArticleId).FirstOrDefault().Title;
+            vm.Content = _context.Articles.Where(i => i.ArticleId == articles.ArticleId).FirstOrDefault().Content;
+
+            return View(vm);
         }
         public IActionResult Sidebar()
         {
