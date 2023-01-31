e59862f3ab518639f3a4f0bdf026502e923423a8
Tibame_T14_05_SandraLee <jame6208@gmail.com>
2023-02-01 00:08:17 +0800

    [新增]ClaimExtension Class的GetMemberId

---
+++ WuliKaWu/WuliKaWu/Extensions/ClaimExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace WuliKaWu.Extensions
+{
+    public static class ClaimExtension
+    {
+        public static int GetMemberId(this IEnumerable<Claim> claims)
+        {
+            return int.TryParse(claims.FirstOrDefault(x => x.Type == "Id")?.Value, out var memberId) ? memberId : -1;
+        }
+    }
+}
