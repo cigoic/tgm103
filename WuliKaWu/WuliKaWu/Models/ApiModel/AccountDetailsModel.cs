8aaecdc3bb00a1e5ef356e952255c67a377e8d96
Jim Chiu <liang.case@gmail.com>
2023-02-02 00:41:17 +0800

    [更新] 會員登入功能, 調整註冊功能

--- WuliKaWu/WuliKaWu/Models/ApiModel/AccountDetailsModel.cs
+++ WuliKaWu/WuliKaWu/Models/ApiModel/AccountDetailsModel.cs
@@ -4,8 +4,10 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string DisplayName { get; set; }
+
+        //public string DisplayName { get; set; }
         public string Email { get; set; }
+
         public string CurrentPwd { get; set; }
         public string NewPwd { get; set; }
         public string ConfirmPwd { get; set; }
