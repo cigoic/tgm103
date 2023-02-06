977de19b66b7173a34f375f4967cbd32a6ea40f7
LAPTOP-PATF7J21\Tibame_T14 <jackymamawang@gmail.com>
2023-02-06 16:16:22 +0800

    [修正]商品新增

---
+++ WuliKaWu/WuliKaWu/Controllers/Api/CategoriesApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WuliKaWu.Data;
+using WuliKaWu.Models.ApiModel;
+
+namespace WuliKaWu.Controllers.Api
+{
+    [Route("api/Category/[action]")]
+    [ApiController]
+    public class CategoriesApiController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public CategoriesApiController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CategoriesApi
+        [HttpGet]
+        public List<CategoryApiModel> GetAll()
+        {
+            return _context.Categories.Select(x=> new CategoryApiModel { 
+                Id = x.CategoryId,
+                Type = x.Type
+            }).ToList();
+        }
+    }
+}
---
+++ WuliKaWu/WuliKaWu/Controllers/Api/ColorApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WuliKaWu.Data;
+using WuliKaWu.Models.ApiModel;
+
+namespace WuliKaWu.Controllers.Api
+{
+    [Route("api/Color/[action]")]
+    public class ColorApiController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ColorApiController(ShopContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public List<ColorApiModel> GetAll()
+        {
+            return _context.Colors.Select(x => new ColorApiModel
+            {
+                Id = x.Id,
+                Type = x.Type
+            }).ToList();
+        }
+    }
+}
