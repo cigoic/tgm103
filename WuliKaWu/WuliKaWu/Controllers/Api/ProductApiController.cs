5c02d981feb11ef3248c991a7f6231dda6a7b113
LAPTOP-PATF7J21\Tibame_T14 <jackymamawang@gmail.com>
2023-02-06 17:04:56 +0800

    product index filter-category

--- WuliKaWu/WuliKaWu/Controllers/Api/ProductApiController.cs
+++ WuliKaWu/WuliKaWu/Controllers/Api/ProductApiController.cs
@@ -30,20 +30,23 @@ namespace WuliKaWu.Controllers.Api
         /// </summary>
         /// <returns></returns>
 
-        public List<ProductModel> GetAll()
+        public List<ProductReadModel> GetAll()
         {
-            throw new NotImplementedException();
-            //return _context.Products.Select(x => new ProductModel
-            //{
-            //    ProductName = x.ProductName,
-            //    Color = x.Color,
-            //    PicturePath = x.PicturePath,
-            //    Price = x.Price,
-            //    ProductId = x.ProductId,
-            //    Size = x.Size.GetDescriptionText(),
-            //    Discount = x.Discount.HasValue ? x.Discount.Value > 0 ? true : false : false,
-            //    SellingPrice = x.SellingPrice.ToString() ?? ""
-            //}).ToList();
+
+            return _context.Products.Include(x=> x.Colors).Include(x=>x.Pictures).Include(x=>x.Tags).Select(x => new ProductReadModel
+            {
+                ProductName = x.ProductName,
+                CategoryId = x.CategoryId,
+                Colors = x.Colors.Select(x => x.Id).ToList(),
+                Pictures = x.Pictures.Select(x => x.PicturePath).ToList(),
+                Price = x.Price,
+                ProductId = x.ProductId,
+                Size = x.Size,
+                Discount = x.SellingPrice.HasValue ? true : false,
+                SellingPrice = x.SellingPrice,
+                Comment = x.Comment,
+                Tags = x.Tags.Select(x => x.Id).ToList()
+            }).ToList();
         }
 
         /// <summary>
@@ -139,8 +142,8 @@ namespace WuliKaWu.Controllers.Api
                 Price = data.Price,
                 SellingPrice = data.SellingPrice,
                 Size = (Size)data.SizeId,
-                Colors = data.ColorIds.Select(x => new Data.Color { Id = x }).ToList(),
-                Tags = data.TagIds.Select(x => new Data.Tag { Id = x }).ToList(),
+                Colors = _context.Colors.Where(x=> data.ColorIds.Contains(x.Id)).ToList(),
+                Tags = _context.Tags.Where(x=> data.TagIds.Contains(x.Id)).ToList()
             };
 
             if (data.Pictures != null)
