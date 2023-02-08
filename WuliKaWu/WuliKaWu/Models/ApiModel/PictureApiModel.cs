fa5c4de447865c4fe3e3943e8d7fca5b9b70148b
Aiwen <a0928777097@gmail.com>
2023-02-08 21:11:02 +0800

    [修正]修復商品刪除按鍵的連動(之後要補上sweetalert)

---
+++ WuliKaWu/WuliKaWu/Models/ApiModel/PictureApiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WuliKaWu.Models.ApiModel
+{
+    public class PictureApiModel
+    {
+        public int PictureId { get; set; }
+
+        public string PicturePath { get; set; }
+    }
+}
