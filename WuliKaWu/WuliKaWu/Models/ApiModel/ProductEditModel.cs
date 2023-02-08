8ba666cabc5589abfcbfa8335c38e032649db02f
Aiwen <a0928777097@gmail.com>
2023-02-08 21:12:32 +0800

    [修改]將EditModel名稱更名為ProductEditModel

---
+++ WuliKaWu/WuliKaWu/Models/ApiModel/ProductEditModel.cs
@@ -0,0 +1,18 @@
+﻿using static WuliKaWu.Data.Enums.Common;
+
+namespace WuliKaWu.Models.ApiModel
+{
+    public class ProductEditModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public List<int> Color { get; set; }
+        public Size Size { get; set; }
+        public int CategoryId { get; set; }
+        public List<string> Pictures { get; set; }
+        public decimal Price { get; set; }
+        public decimal? SellingPrice { get; set; }
+        public List<int> Tag { get; set; }
+        public string Comment { get; set; }
+    }
+}
