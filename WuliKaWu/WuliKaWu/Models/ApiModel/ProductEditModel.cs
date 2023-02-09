ab9c5b788fe9cd49c1ad62a89cb8b9f0d79d3a8d
Aiwen <a0928777097@gmail.com>
2023-02-09 18:43:49 +0800

    [修改]productEditModel欄位名稱

--- WuliKaWu/WuliKaWu/Models/ApiModel/ProductEditModel.cs
+++ WuliKaWu/WuliKaWu/Models/ApiModel/ProductEditModel.cs
@@ -6,13 +6,13 @@ namespace WuliKaWu.Models.ApiModel
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public List<int> Color { get; set; }
-        public Size Size { get; set; }
+        public List<int> ColorIds { get; set; }
+        public Size SizeId { get; set; }
         public int CategoryId { get; set; }
         public List<string> Pictures { get; set; }
         public decimal Price { get; set; }
         public decimal? SellingPrice { get; set; }
-        public List<int> Tag { get; set; }
+        public List<int> TagIds { get; set; }
         public string Comment { get; set; }
     }
 }
