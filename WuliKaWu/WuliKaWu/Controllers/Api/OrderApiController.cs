631f29f99427d301c91154120244c9b33744aa60
Tibame_T14_05_SandraLee <jame6208@gmail.com>
2023-02-13 04:20:03 +0800

    [新增]OrderController、OrderApiController [新增]OrderApiModel

---
+++ WuliKaWu/WuliKaWu/Controllers/Api/OrderApiController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WuliKaWu.Data;
+using WuliKaWu.Extensions;
+using WuliKaWu.Models.ApiModel;
+
+namespace WuliKaWu.Controllers.Api
+{
+    [Route("api/order/[action]")]
+    [ApiController]
+    public class OrderApiController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public OrderApiController(ShopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<OrderApiModel>> GetOrderAsync(int checkoutId)
+        {
+            var myId = User.Claims.GetMemberId();
+            try
+            {
+                var order = _context.Orders
+                    .Where(o => o.MemberId == myId)
+                    .Select(o => new OrderApiModel
+                    {
+                        MemberId = myId,
+                        OrderId = o.OrderId,
+                        OrderDate = o.OrderDate,
+                        ShippingDate = o.ShippingDate,
+                        Status = o.Status,
+                        Type = o.Type,
+                        Recipient = o.Recipient,
+                        ShippingAddress = o.ShippingAddress,
+                        ContactPhone = o.ContactPhone,
+                        Memo = o.Memo
+                    });
+                return await order.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
