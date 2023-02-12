903b6b15b48d1353a2a5c2419c96f71cfdc80853
Jim Chiu <liang.case@gmail.com>
2023-02-13 03:07:56 +0800

    [加入] 文章編輯功能

---
+++ WuliKaWu/WuliKaWu/Models/ArticleUpdateModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WuliKaWu.Models
+{
+    public class ArticleUpdateModel
+    {
+        public int ArticleId { get; set; }
+        public int MemberId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
