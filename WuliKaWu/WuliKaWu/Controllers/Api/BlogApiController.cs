e6dbbc644263e7c616bd65f895260d39b41590a2
Jim Chiu <liang.case@gmail.com>
2023-02-12 16:09:46 +0800

    [更正] CK Editor 上傳圖片仍有問題，前次 commit 修改的效果仍舊失敗

--- WuliKaWu/WuliKaWu/Controllers/Api/BlogApiController.cs
+++ WuliKaWu/WuliKaWu/Controllers/Api/BlogApiController.cs
@@ -360,7 +360,7 @@ namespace WuliKaWu.Controllers.Api
                 return Results.NotFound(new { Status = false, Message = "圖片媒體有誤，無法上傳！" });
 
             //var rootPath = $@"{_env.WebRootPath}\images\ckeditor";
-            var rootPath = $@"{_env.WebRootPath}\ckfinder\userfiles\files";
+            var rootPath = $@"/images/ckeditor";
             if (!Directory.Exists(rootPath))
             {
                 Directory.CreateDirectory(rootPath);
@@ -374,7 +374,9 @@ namespace WuliKaWu.Controllers.Api
                 //fileName = Path.GetFileName(image.FileName);
                 if (string.IsNullOrEmpty(fileName)) return Results.NotFound(new { success = false, uploaded = false });
 
-                filePath = Path.Combine(rootPath, fileName);
+                //filePath = Path.Combine(rootPath, fileName);
+                filePath = Path.Combine(
+                   Directory.GetCurrentDirectory(), "wwwroot/images/ckeditor/", fileName);
                 // TODO 將圖片位置存入資料庫！
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
@@ -382,11 +384,17 @@ namespace WuliKaWu.Controllers.Api
                 }
             }
 
+            var url = $"{"/images/ckeditor/"}{fileName}";
+
             return Results.Ok(new
             {
-                fileName = $"{fileName}",
-                uploaded = true,
-                url = $"https://localhost:7082/ckfinder/userfiles/files"
+                Uploaded = 1,
+                FileName = $"{fileName}",
+                Url = url,
+                //Error = new
+                //{
+                //    Message = "圖檔上傳失敗!"
+                //}
                 //resourceType = "Images",//"Files",
                 //currentFolder = new
                 //{
